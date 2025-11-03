b0fc6f0a49950f0aa248d0ad4a9a9b7f3cbade50
Valeria Reyes <valeriaromero0604@gmail.com>
2025-11-03 16:46:19 -0600

    se agrego el dto

---
+++ SysLaboratorioClinico.Domain/DTO/DoctorDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysLaboratorioClinico.Domain.DTO
+{
+    public class DoctorDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Especialidad { get; set; } = string.Empty;
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+        public string LicenciaMedica { get; set; } = string.Empty;
+        public bool Estado { get; set; }
+        public int IdUsuario { get; set; } // Para mantener consistencia con la entidad
+    }
+}
